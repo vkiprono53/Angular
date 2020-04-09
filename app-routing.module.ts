42f22aa51267481870d028497389a4174663af4b
vkiprono53 <kipronovincent53@gmail.com>
2020-04-09 23:48:30 +0300

    My first commit

---
+++ app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {ErrorComponent} from './components/error/error.component';
+import {AboutComponent} from './components/about/about.component';
+import {ContactComponent} from './components/contact/contact.component';
+import {ServicesComponent} from './components/services/services.component';
+
+
+const routes: Routes = [
+  {path : '' , component: HomeComponent},
+  {path : 'about' , component: AboutComponent},
+  {path : 'contact' , component: ContactComponent},
+  {path : 'services' , component: ServicesComponent},
+  {path : '**' , component: ErrorComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent,
+    ErrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
